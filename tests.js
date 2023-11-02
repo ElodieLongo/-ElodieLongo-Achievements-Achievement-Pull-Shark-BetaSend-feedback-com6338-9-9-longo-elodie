ba6b0b63b83be38765cc7c7baa6006a17ff8f028
“Elodie <mail.elodielongo@gmail.com>
2023-11-03 00:09:34 +0100

    firdt commit

--- tests.js
+++ tests.js
@@ -123,107 +123,175 @@ function runTests() {
   mocha.setup("bdd");
   const expect = chai.expect;
 
-  describe("Hamburger Menu", function() {
-    const hamburgerBtn = document.querySelector('.hamburger-btn')
-    const hamburgerMenu = document.querySelector('.hamburger-menu')
-    const openMenu = () => {
-      hamburgerMenu.classList.add('show-menu')
-      hamburgerBtn.setAttribute('aria-expanded', true)
+  describe("Weather App", function () {
+    const getLocalTime = sec => new Date(sec * 1000)
+      .toLocaleTimeString('en-US', {hour: 'numeric', minute: 'numeric'})
+      .toLowerCase()
+    const weatherInfo = {
+      "coord": {
+        "lon": 139.6917,
+        "lat": 35.6895
+      },
+      "weather": [
+        {
+          "id": 801,
+          "main": "Clouds",
+          "description": "few clouds",
+          "icon": "02d"
+        }
+      ],
+      "base": "stations",
+      "main": {
+        "temp": 55.42,
+        "feels_like": 52.74,
+        "temp_min": 50.67,
+        "temp_max": 58.59,
+        "pressure": 1020,
+        "humidity": 44
+      },
+      "visibility": 10000,
+      "wind": {
+        "speed": 4.61,
+        "deg": 90
+      },
+      "clouds": {
+        "all": 20
+      },
+      "dt": 1647843122,
+      "sys": {
+        "type": 2,
+        "id": 268395,
+        "country": "JP",
+        "sunrise": 1647809057,
+        "sunset": 1647852769
+      },
+      "timezone": 32400,
+      "id": 1850144,
+      "name": "Tokyo",
+      "cod": 200
     }
-    const closeMenu = () => {
-      document.activeElement.blur()
-      hamburgerMenu.classList.remove('show-menu')
-      hamburgerBtn.setAttribute('aria-expanded', false)
+
+    const getWeatherHTML = () =>
+      document.getElementById('weather-app').innerHTML.toLowerCase()
+
+    async function stubWeatherFetch(returnJSON, query, status) {
+      const fetchStub = sinon.stub(window, 'fetch')
+        .resolves({ json: sinon.stub().resolves(returnJSON), status: status || 200 })
+      document.querySelector('input').value = query
+      document.querySelector('#weather-app form button').click()
+      expect(fetchStub.called).to.be.true
+      expect(fetchStub.firstCall.args[0].includes(query)).to.be.true
+      await (() => {
+        return new Promise(res => {
+          setTimeout(res, 0)
+        })
+      })();
     }
-    const pressEscape = () => document.dispatchEvent(new KeyboardEvent('keyup', {
-      key: "Escape",
-      keyCode: 27,
-      which: 27,
-      code: "Escape",
-     }))
-    before(() => {
-      document.querySelector('style').textContent += `
-        .hamburger-btn {
-          display: block;
-        }
-        .hamburger-menu li {
-          visibility: hidden;
-        }
-        .hamburger-menu.show-menu li {
-          visibility: visible;
-        }
-      `
+
+    before(async () => {
+      document
+        .querySelector('#weather-app form')
+        .addEventListener('submit', e => e.preventDefault())
+      await stubWeatherFetch(weatherInfo, 'tokyo, jp')
     })
-    after(() => {
+
+    after(function () {
+      sinon.restore()
       testBtn.textContent = 'Close Tests'
       testBtn.disabled = false
     })
-    afterEach(closeMenu)
-    const menuIsOpen = () => hamburgerMenu.classList.contains('show-menu')
-    it('Should not have menu open when page loads', () => {
-      expect(menuIsOpen()).to.be.false
+
+    it('should display "Location Not Found" if no location is found', async function () {
+      sinon.restore()
+      await stubWeatherFetch({ data: { cod: 404 } }, 'askjdnfks', 404)
+      expect(getWeatherHTML().includes('location not found')).to.be.true
     })
-    it('Should open menu when clicking the hamburger button', () => {
-      hamburgerBtn.click()
-      expect(menuIsOpen()).to.be.true
+    it('should clear the input value after searching for weather data', () => {
+      expect(document.querySelector('#weather-app input').value).to.eq("")
     })
-    it('Should set aria-expanded="true" to hamburger button when menu is opened', () => {
-      hamburgerBtn.click()
-      expect(hamburgerBtn.getAttribute('aria-expanded')).to.eq('true')
+    it('should not display "Location not found" after entering a valid location', async () => {
+      sinon.restore()
+      await stubWeatherFetch(weatherInfo, 'tokyo, jp')
+      expect(getWeatherHTML().includes('location not found')).to.be.false
     })
-    it('Should close menu when clicking the hamburger button', () => {
-      openMenu()
-      hamburgerBtn.click()
-      expect(menuIsOpen()).to.be.false
+    it('should display city', function () {
+      const weatherHTML = getWeatherHTML()
+      expect(weatherHTML.includes('tokyo')).to.be.true
+      expect(weatherHTML.includes('jp')).to.be.true
     })
-    it('Should set aria-expanded="false" to hamburger button when menu is closed', () => {
-      openMenu()
-      hamburgerBtn.click()
-      expect(hamburgerBtn.getAttribute('aria-expanded')).to.eq('false')
+    it('should display working map link', function () {
+      const link = document.querySelector('#weather-app a')
+      expect(link).to.exist
+      expect(link.textContent.toLowerCase()).to.eq('click to view map')
+      expect(link.href).to.eq('https://www.google.com/maps/search/?api=1&query=35.6895,139.6917')
     })
-    it('Should NOT open menu when clicking things other than the hamburger button', () => {
-      document.querySelector('header').click()
-      expect(menuIsOpen()).to.be.false
-      document.querySelector('.banner').click()
-      expect(menuIsOpen()).to.be.false
-      document.querySelector('footer').click()
-      expect(menuIsOpen()).to.be.false
-      document.querySelector('section button').click()
-      expect(menuIsOpen()).to.be.false
+    it('should display condition icon', function () {
+      const img = document.querySelector('#weather-app img')
+      expect(img).to.exist
+      expect(img.src).to.eq('https://openweathermap.org/img/wn/02d@2x.png')
     })
-    it('Should close menu when clicking outside of the menu', () => {
-      openMenu()
-      document.querySelector('.banner').click()
-      expect(menuIsOpen()).to.be.false
-      openMenu()
-      document.querySelector('footer').click()
-      expect(menuIsOpen()).to.be.false
-      openMenu()
-      document.querySelector('section').click()
-      expect(menuIsOpen()).to.be.false
+    it('should display condition', function () {
+      expect(getWeatherHTML().includes('few clouds')).to.be.true
     })
-    it('Should NOT close menu when clicking inside of the menu', () => {
-      openMenu()
-      document.querySelector('.hamburger-menu').click()
-      document.querySelector('.hamburger-menu li').click()
-      document.querySelector('.hamburger-menu li:nth-of-type(3) a').click()
-      expect(menuIsOpen()).to.be.true
+    it('should display current temp', function () {
+      expect(getWeatherHTML().includes('55.42')).to.be.true
     })
-    it('Should close menu when pressing escape and menu items are NOT focused', () => {
-      openMenu()
-      pressEscape()
-      expect(menuIsOpen()).to.be.false
+    it('should display current "feels like" temp', function () {
+      expect(getWeatherHTML().includes('52.74')).to.be.true
     })
-    it('Should close menu and focus hamburger button when pressing escape and menu items are focused', () => {
-      openMenu()
-      document.querySelector('.hamburger-menu a').focus()
-      pressEscape()
-      expect(document.activeElement).to.eq(hamburgerBtn)
-      expect(menuIsOpen()).to.be.false
+    it('should display updated time', function () {
+      expect(getWeatherHTML().includes(getLocalTime(weatherInfo.dt))).to.be.true
     })
-    it('Should NOT open menu when pressing escape', () => {
-      pressEscape()
-      expect(menuIsOpen()).to.be.false
+    it("should not display previous location's weather info after searching for a new location", async () => {
+      sinon.restore()
+      await stubWeatherFetch({
+          "coord": {
+              "lon": -157.8583,
+              "lat": 21.3069
+          },
+          "weather": [
+              {
+                  "main": "Clouds",
+                  "description": "broken clouds",
+                  "icon": "04n"
+              }
+          ],
+          "main": {
+              "temp": 73.67,
+              "feels_like": 74.88,
+          },
+          "dt": 1647845849,
+          "sys": {
+              "country": "US",
+          },
+          "id": 5856195,
+          "name": "Honolulu",
+        }, 'honolulu, us')
+
+      const weatherHTML = getWeatherHTML()
+      expect(weatherHTML.includes('tokyo')).to.be.false
+      expect(weatherHTML.includes('jp')).to.be.false
+      const link = document.querySelector('#weather-app a')
+      expect(link).to.exist
+      expect(link.textContent.toLowerCase()).to.eq('click to view map')
+      expect(link.href).to.not.eq('https://www.google.com/maps/search/?api=1&query=35.6895,-157.8583')
+      const img = document.querySelector('#weather-app img')
+      expect(img).to.exist
+      expect(img.src).to.not.eq('https://openweathermap.org/img/wn/02d@2x.png')
+      expect(getWeatherHTML().includes('few clouds')).to.be.false
+      expect(getWeatherHTML().includes('55.42')).to.be.false
+      expect(getWeatherHTML().includes('52.74')).to.be.false
+      expect(getWeatherHTML().includes(getLocalTime(weatherInfo.dt))).to.be.false
+
+      expect(weatherHTML.includes('honolulu')).to.be.true
+      expect(weatherHTML.includes('us')).to.be.true
+      expect(link.textContent.toLowerCase()).to.eq('click to view map')
+      expect(link.href).to.eq('https://www.google.com/maps/search/?api=1&query=21.3069,-157.8583')
+      expect(img.src).to.eq('https://openweathermap.org/img/wn/04n@2x.png')
+      expect(getWeatherHTML().includes('broken clouds')).to.be.true
+      expect(getWeatherHTML().includes('73.67')).to.be.true
+      expect(getWeatherHTML().includes('74.88')).to.be.true
+      expect(getWeatherHTML().includes(getLocalTime(1647845849))).to.be.true
     })
   });
 
